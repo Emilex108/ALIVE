f1e303da6eaf5e98019819cf2dd83a58ddb60a7a
Blain <e1831622@cmaisonneuve.qc.ca>
2020-02-27 07:40:46 -0500

    autopilotJava update

--- Arduino/AutoPilotJava/AutoPilotJava/AutoPilotJava.ino
+++ Arduino/AutoPilotJava/AutoPilotJava/AutoPilotJava.ino
@@ -1,11 +1,14 @@
 /*
 AutoPilot Script on Arduino for ALIVE Project made by Émile Gagné & Guillaume Blain
 */
+//librarie for the lcd display
+#include <LiquidCrystal.h>
 //Libraries for the gyroscope (angle)
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 //setting the gyroscope
 MPU6050 mpu6050(Wire);
+LiquidCrystal lcd(12, 13, 5, 4, 3, 2);
 //Setting the Arduino pins for the motors
 const int motorPin1  = 11;
 const int motorPin2  = 10;
@@ -13,14 +16,15 @@ const int motorPin3  = 9;
 const int motorPin4  = 6;
 int SPEED = 110;
 const int ROTATION_SPEED = 100;
+const int ROTATION_FORCE = 65;
 //Sensors related settings
 const int nbSensors = 3;
 const int pins[nbSensors*2] = {23,22,38,39,37,36};
 //This is remotely enabled by bluetooth to active the autopilot script
 boolean autopilot = false;
 //Those are the detections limits in centimeters (It will detect an object if closer to 15 cm) The detectionB is meant to debug curved or angular objects
-const int detection = 10;
-const int detectionAvant = 8;
+const int detection = 40;
+const int detectionAvant = 35;
 const int detectionB = 400;
 //Definition of the distances of the captors (TODO Replace by Array to be more flexible)
 int distAvant,distGauche,distDroite;
@@ -51,9 +55,15 @@ void setup(){
   pinMode(pins[5], INPUT);
   //Serial on 115200 is for the bluetooth (TODO Try putting it back to 9600 or around, to avoid Unicode problem)
   Serial.begin(115200);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+   lcd.print("calculating...");
+   //Wire and mpu are for the gyroscope (angle)
   Wire.begin();
   mpu6050.begin();
-  mpu6050.calcGyroOffsets();
+  mpu6050.calcGyroOffsets(true);
+  lcd.clear();
+  lcd.print("Ready to go !");
 }
 
 
@@ -72,6 +82,14 @@ void loop(){
     nbDemiTour = angle/180;
     angle = angle%180;
   }
+ 
+   //these methods are mostly for debugging, they add a lot of delay
+   //do not use two at the same time
+   
+  // displayCollision();
+  // displayAccel();
+  // displayAngle();
+  
   //Checks to see if there are any commands over BT available and that Autopilot hasn't been set ON (TODO Check to replace with Switch)
   if (Serial.available()>0) {
     val = Serial.read();
@@ -80,10 +98,18 @@ void loop(){
       avancer();
     }else if(val == 2){
       droite();
+    }else if(val == 12){
+      avancerDroite();
+    }else if(val == 32){
+      reculerDroite();
     }else if(val == 3){
       reculer();
     }else if(val == 4){
       gauche();
+    }else if(val == 14){
+      avancerGauche();
+    }else if(val == 34){
+      reculerGauche();
     }else if(val == 0){
       arreter();
     }else if(val == 100){
@@ -112,6 +138,10 @@ void loop(){
       autopilot = true;
     }else if(val == 6){
       autopilot = false;
+    }else if(val == 8){
+      delay(100);
+      val = Serial.read();
+      goToAngle(val);
     }
   }
 
@@ -123,24 +153,24 @@ void loop(){
     if(d && a && g){
       reculer();
     }else if(g && a && !d){
-      droite();
+     avancerDroite();
     }else if(!g && a && d){
-      gauche();
+       avancerGauche();
     }else if(g && !a && d){
       avancer();
     }else if(!g && !a && d){
-      gauche();
+      avancerGauche();
     }else if (g && !a && !d){
-      droite();
+     avancerDroite();
     }else if(!g && !a && !d){
       avancer();
     }else if(!g && a && !d){
       distDroite = getDistance(2);
       distGauche = getDistance(1);
       if(distDroite < distGauche){
-        gauche();
+        avancerGauche();
       }else if(distGauche < distDroite){
-        droite();
+        avancerDroite();
       }else{
         reculer();
       }
@@ -155,10 +185,34 @@ void avancer(){
   analogWrite(motorPin3, 0);
   analogWrite(motorPin4, SPEED);
 }
+void avancerDroite(){
+   analogWrite(motorPin1, SPEED - ROTATION_FORCE);
+  analogWrite(motorPin2, 0);
+  analogWrite(motorPin3, 0);
+  analogWrite(motorPin4, SPEED);
+}
+void avancerGauche(){
+   analogWrite(motorPin1, SPEED);
+  analogWrite(motorPin2, 0);
+  analogWrite(motorPin3, 0);
+  analogWrite(motorPin4, SPEED - ROTATION_FORCE);
+}
 void reculer(){
   analogWrite(motorPin1, 0);
   analogWrite(motorPin2, SPEED);
   analogWrite(motorPin3, SPEED);
+  analogWrite(motorPin4, 0);
+}
+void reculerDroite(){
+   analogWrite(motorPin1, 0);
+  analogWrite(motorPin2, SPEED - ROTATION_FORCE);
+  analogWrite(motorPin3, SPEED);
+  analogWrite(motorPin4, 0);
+}
+void reculerGauche(){
+   analogWrite(motorPin1, 0);
+  analogWrite(motorPin2, SPEED);
+  analogWrite(motorPin3, SPEED - ROTATION_FORCE);
   analogWrite(motorPin4, 0);
 }
 void arreter(){
@@ -222,3 +276,78 @@ byte getDistance(int i){
   // Calculating the distance
   return duration*0.034/2;
 }
+void goToAngle(double angle){
+  for(int i =0 ; i<5 ; i++){
+    delay(10);
+   mpu6050.update();
+  double angleCurrent = mpu6050.getGyroAngleX();
+  double ecart = 0.5;
+  while(!(angleCurrent-ecart <= angle && angle <= angleCurrent+ecart)){
+    mpu6050.update();
+    angleCurrent = mpu6050.getGyroAngleX();
+    if(angleCurrent >= angle){
+      droite();
+      lcd.setCursor(0, 1);
+    }
+    if(angleCurrent <= angle){
+       gauche();
+      lcd.setCursor(0, 1);
+    }
+  }
+  arreter();
+  }
+}
+
+//**************LCD DISPLAY******************//
+void displayCollision(){
+   lcd.clear();
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(1, 0);
+  // print the number of seconds since reset:
+  lcd.print("detecion : ");
+  String toDisplay = "";
+  if(collisionDroite()){
+     toDisplay += "OO";
+  } else {
+    toDisplay += "  ";
+  }
+  if(collisionAvant()){
+     toDisplay += "     OO";
+  } else {
+    toDisplay += "       ";
+  }
+   if(collisionGauche()){
+     toDisplay += "     OO";
+  } else {
+    toDisplay += "       ";
+  }
+  lcd.setCursor(0, 1);
+   lcd.print(toDisplay);
+  delay(50);
+}
+
+void displayAngle(){
+   mpu6050.update();
+   lcd.clear();
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 0);
+  // print the number of seconds since reset:
+  lcd.print("Angle : ");
+  lcd.print(mpu6050.getGyroAngleX());
+  delay(50);
+}
+
+void displayAccel(){
+   mpu6050.update();
+   lcd.clear();
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 0);
+  // print the number of seconds since reset:
+  lcd.print("Accel : ");
+  lcd.print(mpu6050.getAccZ());
+  delay(300);
+}
+//**************LCD DISPLAY******************//
